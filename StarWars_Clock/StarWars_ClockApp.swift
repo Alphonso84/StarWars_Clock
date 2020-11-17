750a2b33d80e0b7cc94b3d1b20262cd8d288d839
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-11-16 21:48:03 -0800

    Initial Commit

---
+++ StarWars_Clock/StarWars_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWars_ClockApp.swift
+//  StarWars_Clock
+//
+//  Created by Alphonso Sensley II on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct StarWars_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
